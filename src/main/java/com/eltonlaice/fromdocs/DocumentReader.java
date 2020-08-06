bf5eea7e8da881b9ca1e73b2560edffe68a6e107
Elton Laice <me@eltonlaice.com>
2020-08-07 01:33:01 +0200

    Create switch selector for document types

--- src/main/java/com/eltonlaice/fromdocs/DocumentReader.java
+++ src/main/java/com/eltonlaice/fromdocs/DocumentReader.java
@@ -1,33 +1,93 @@
 package com.eltonlaice.fromdocs;
 
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 public class DocumentReader {
 	private String type;
 	
 	private String file;
-
-	public DocumentReader() {}
 	
+	private Tesseract tesseract;
+	
+	private Document document;
+	
+	/**
+	 * 
+	 * @param file
+	 * @param type
+	 */
 	public DocumentReader(String file, String type) {
 		this.type = type;
 		this.file = file;
+		init();
 	}
 	
-	public DocumentReader(String file) {
-		this.file = file;
+	/**
+	 * 
+	 */
+	public void init() {
+		DocumentProcess documentProcess = new DocumentProcess();
+    	File image = new File(this.file);
+    	this.tesseract = new Tesseract();
+    	this.tesseract.setDatapath("src/main/resources/tessdata");
+    	this.tesseract.setLanguage("eng");
+    	this.tesseract.setPageSegMode(1);
+    	this.tesseract.setOcrEngineMode(1);
+    	
+    	try {
+			String data = tesseract.doOCR(image);
+	    	if(this.type.equalsIgnoreCase(Config.NUIT)) {
+	    		documentProcess.processNuit(data);
+	    	}if (this.type.equalsIgnoreCase(Config.BI) || this.type.equalsIgnoreCase(Config.ID)) {
+	    		documentProcess.processId(data);
+			}else if (this.type.equalsIgnoreCase(Config.PASSPORT)) {
+				documentProcess.processPassport(data);
+			}else {
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Document getDocument() {
+		return document;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * 
+	 * @param type
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getFile() {
 		return file;
 	}
 
+	/**
+	 * 
+	 * @param file
+	 */
 	public void setFile(String file) {
 		this.file = file;
 	}
