ee72b469cd96b7392d1f739f2b5cf87fe23d45d3
Elton Laice <me@eltonlaice.com>
2020-08-30 16:39:06 +0200

    Update enum DocumentType

--- src/main/java/com/eltonlaice/fromdocs/DocumentType.java
+++ src/main/java/com/eltonlaice/fromdocs/DocumentType.java
@@ -1,11 +1,8 @@
 package com.eltonlaice.fromdocs;
 
-public class Config {
-	public static String BI = "BI";
-	
-	public static String ID = "ID";
-	
-	public static String NUIT = "NUIT";
-	
-	public static String PASSPORT = "PASSPORT";
+public enum DocumentType {
+	BI,
+	ID,
+	NUIT,
+	PASSPORT
 }
